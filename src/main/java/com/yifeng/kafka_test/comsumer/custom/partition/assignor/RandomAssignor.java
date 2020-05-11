4c4ed51c9f4c43628fc0b104f921dd3c175f8060
YifengGuo <gyf940517@gmail.com>
2020-05-11 15:13:07 +0800

    customized random partition assignor for consumer

--- src/main/java/com/yifeng/kafka_test/comsumer/custom/partition/assignor/RandomAssignor.java
+++ src/main/java/com/yifeng/kafka_test/comsumer/custom/partition/assignor/RandomAssignor.java
@@ -4,42 +4,74 @@ import org.apache.kafka.clients.consumer.internals.AbstractPartitionAssignor;
 import org.apache.kafka.common.Cluster;
 import org.apache.kafka.common.TopicPartition;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
 
 /**
  * Created by guoyifeng on 5/9/20
+ *
+ * randomly assign a partition of topic to consumer which has subscribed this topic
+ * to use this, declare in consumer properties initialization:
+ * properties.put(ConsumerConfig.PARTITION_ASSIGNMENT_STRATEGY_CONFIG, RandomAssignor.class.getName());
  */
 public class RandomAssignor extends AbstractPartitionAssignor {
     public RandomAssignor() {
         super();
     }
 
-    @Override
-    public Subscription subscription(Set<String> topics) {
-        return super.subscription(topics);
+    /**
+     * get topic to consumer which subscribed this topic mappings
+     * @param consumerMetadata
+     * @return topic -> [consumer1, consumer2 ...]
+     */
+    private Map<String, List<String>> consumersPerTopic(Map<String, Subscription> consumerMetadata) {
+        Map<String, List<String>> res = new HashMap<>();
+        for (Map.Entry<String, Subscription> subscriptionEntry : consumerMetadata.entrySet()) {
+            String consumerId = subscriptionEntry.getKey();
+            for (String topic : subscriptionEntry.getValue().topics()) {
+                put(res, topic, consumerId);
+            }
+        }
+        return res;
     }
 
     /**
      * logic for assigning partitions for consumer in group
-     * @param metadata
+     * @param partitionsPerTopic
      * @param subscriptions
      * @return
      */
     @Override
-    public Map<String, Assignment> assign(Cluster metadata, Map<String, Subscription> subscriptions) {
-        return super.assign(metadata, subscriptions);
-    }
-
-    @Override
-    public void onAssignment(Assignment assignment) {
-        super.onAssignment(assignment);
-    }
-
-    @Override
     public Map<String, List<TopicPartition>> assign(Map<String, Integer> partitionsPerTopic, Map<String, Subscription> subscriptions) {
-        return null;
+        Map<String, List<TopicPartition>> assignment = new HashMap<>();
+        Map<String, List<String>> consumersPerTopic = consumersPerTopic(subscriptions);
+        // initial assignment with empty partition list
+        for (String consumerId : consumersPerTopic.keySet()) {
+            assignment.put(consumerId, new ArrayList<>());
+        }
+
+        // distribute partition to random consumer which has subscribed its topic
+        for (Map.Entry<String, List<String>> topicEntry : consumersPerTopic.entrySet()) {
+            String topic = topicEntry.getKey();
+            List<String> consumersForTopic = topicEntry.getValue(); // consumers which have subscribed this topic
+            int consumerNum = consumersForTopic.size();
+
+            // sanity check
+            int partitionsNum = partitionsPerTopic.get(topic);
+            if (partitionsNum == 0) {
+                continue; // means this topic has no partition
+            }
+
+            // get all partitions info of current topic
+            List<TopicPartition> partitions = AbstractPartitionAssignor.partitions(topic, partitionsNum);
+            // distribute partitions randomly to consumers
+            for (TopicPartition partition : partitions) {
+                int rand = ThreadLocalRandom.current().nextInt(consumerNum);
+                String randConsumer = consumersForTopic.get(rand);
+                assignment.get(randConsumer).add(partition);
+            }
+        }
+        return assignment;
     }
 
     /**
